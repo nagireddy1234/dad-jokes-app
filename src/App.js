0a48093c74274e1f2083a5b55abc214fe9217c19
Nagireddy P <nagireddy.panditi@gmail.com>
2020-03-21 19:40:36 +0530

    added dynamic color and emojis to joke

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import './App.css';
 import axios from 'axios';
 import uuid from "react-uuid";
 import Joke from "./Components/Joke";
-import laughing from "./Images/laughing.png"
+import laughing from "./Images/laughing.png";
+
 //  import JokeList from './Components/JokeList';
+
+//  <i class="em em-laughing" aria-role="presentation" aria-label="SMILING FACE WITH OPEN MOUTH AND TIGHTLY-CLOSED EYES"></i> 
+// <i class="em em-smiley" aria-role="presentation" aria-label="SMILING FACE WITH OPEN MOUTH"></i>
+// <i class="em em-slightly_smiling_face" aria-role="presentation" aria-label="SLIGHTLY SMILING FACE"></i>
+{/* <i class="em em-neutral_face" aria-role="presentation" aria-label="NEUTRAL FACE"></i> */}
+// <i class="em em-confused" aria-role="presentation" aria-label="CONFUSED FACE"></i>
+// <i class="em em-angry" aria-role="presentation" aria-label="ANGRY FACE"></i>
+
 
 export default class App extends Component {
 
@@ -32,17 +41,24 @@ export default class App extends Component {
     })
   }
 
+   handleVotes = (id, delta)=>{
+     this.setState(st=>({
+       jokes:st.jokes.map(joke=>joke.id===id?{...joke, votes:joke.votes+delta}:joke)
+     }))
+
+  }
+
   render() {
     return (
       <div className="App">
         <div className="jokelist-sidebar">
-          <h1 className="jokelist-tile"> <span>Dad</span> Jokes</h1>
+          <h1 className="jokelist-title"> <span>Dad</span> Jokes</h1>
          <img src={laughing} alt="laughingImage" /> 
          <button type="button">Fetch Jokes</button>
         </div>
           <div className="jokelist-jokes">
           {this.state.jokes.map((joke)=>{
-            return <Joke key={uuid()} joke={joke} />
+            return <Joke key={uuid()} joke={joke} upvote={()=>this.handleVotes(joke.id,1)} downvote={()=>this.handleVotes(joke.id,-1)}   />
           })}
           {/* <JokeList /> */}
           </div>
--- src/Components/Joke.js
+++ src/Components/Joke.js
@@ -1,10 +1,66 @@
 import React from 'react';
+import "./Joke.css"
 
 const Joke = (props) => {
   // console.log(props.joke)
+
+ const getColor = () => {
+  if(props.joke.votes>=15) {
+      return "#4caf50";
+  }
+  else if(props.joke.votes>=12){
+     return "#8bc34a";
+  }
+  else if(props.joke.votes>=9){
+    return "#cddc39";
+  }
+  else if(props.joke.votes>=6){
+    return "#ffeb3b";
+  }
+  else if(props.joke.votes>=3){
+    return "#ffc107";
+  }
+  else if(props.joke.votes>=0){
+    return "#ff9800";
+  }
+  else {
+    return "#f44336";
+  }
+}
+
+const getEmoji = () => {
+  if(props.joke.votes>=15) {
+      return <i className="em em-rolling_on_the_floor_laughing"  aria-label="ROLLING ON THE FLOOR LAUGHING"></i>;
+  }
+  else if(props.joke.votes>=12){
+     return <i class="em em-laughing" aria-label="SMILING FACE WITH OPEN MOUTH AND TIGHTLY-CLOSED EYES"></i>;
+  }
+  else if(props.joke.votes>=9){
+    return <i class="em em-smiley" aria-label="SMILING FACE WITH OPEN MOUTH"></i>;
+  }
+  else if(props.joke.votes>=6){
+    return <i class="em em-slightly_smiling_face"  aria-label="SLIGHTLY SMILING FACE"></i>;
+  }
+  else if(props.joke.votes>=3){
+    return  <i class="em em-neutral_face"  aria-label="NEUTRAL FACE"></i>;
+  }
+  else if(props.joke.votes>=0){
+    return <i class="em em-confused"  aria-label="CONFUSED FACE"></i>;
+  }
+  else {
+    return <i class="em em-angry"  aria-label="ANGRY FACE"></i>;
+  }
+}
+
   return (
-    <div className="container">
-      <ul> <li>{props.joke.text}-{props.joke.votes}</li></ul>
+    <div className="joke container">
+      <div className="joke-buttons">
+      <i className='fas fa-arrow-up' onClick={props.upvote}></i>
+      <span className="joke-votes" style={{borderColor:getColor()}}> {props.joke.votes} </span>
+      <i className='fas fa-arrow-down' onClick={props.downvote}></i>
+      </div>
+     <div className="joke-text"> {props.joke.text} </div>
+  <div className="joke-smiley">{getEmoji()}</div>
     </div>
   );
 };
